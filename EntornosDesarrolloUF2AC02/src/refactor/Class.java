5a5526ccf32ed92a738f8fe212a9417738f66f90
loqum <ruben693@gmail.com>
2018-11-05 10:59:48 +0100

    First Commit

---
+++ EntornosDesarrolloUF2AC02/src/refactor/Class.java
@@ -0,0 +1,14 @@
+package refactor;
+
+public class Class {
+	
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 10;
+		
+		int c = Utils.suma(a, b);
+		
+		System.out.println(c);
+	}
+
+}
---
+++ EntornosDesarrolloUF2AC02/src/refactor/Utils.java
@@ -0,0 +1,9 @@
+package refactor;
+
+public class Utils {
+	
+	public static int suma (int a, int b) {
+		return a + b;
+	}
+
+}
